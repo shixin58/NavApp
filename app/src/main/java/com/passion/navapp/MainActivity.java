16f1cdf1af0822b6f1214c2198f1f3a87f41dd8d
shixin <shixin1359@gmail.com>
2022-07-26 09:36:01 +0800

    创建app、libnavcompiler、libnavannotation模块

---
+++ app/src/main/java/com/passion/navapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.passion.navapp;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.passion.navapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding binding;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        navController.navigate(item.getItemId());
+        return !TextUtils.isEmpty(item.getTitle());
+    }
+}
---
+++ libnavcompiler/src/main/java/com/passion/libnavcompiler/NavProcessor.java
@@ -0,0 +1,142 @@
+package com.passion.libnavcompiler;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.google.auto.service.AutoService;
+import com.passion.libnavannotation.ActivityDestination;
+import com.passion.libnavannotation.FragmentDestination;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({"com.passion.libnavannotation.FragmentDestination", "com.passion.libnavannotation.ActivityDestination"})
+public class NavProcessor extends AbstractProcessor {
+    private static final String OUTPUT_FILE_NAME = "destination.json";
+
+    private Messager messager;// 打印日志
+    private Filer filer;// 生成文件
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        messager = processingEnv.getMessager();
+        filer = processingEnv.getFiler();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnv) {
+        Set<? extends Element> fragmentElements = roundEnv.getElementsAnnotatedWith(FragmentDestination.class);
+        Set<? extends Element> activityElements = roundEnv.getElementsAnnotatedWith(ActivityDestination.class);
+        if (!fragmentElements.isEmpty() || !activityElements.isEmpty()) {
+            HashMap<String, JSONObject> destMap = new HashMap<>();
+            handleDestination(fragmentElements, FragmentDestination.class, destMap);
+            handleDestination(activityElements, ActivityDestination.class, destMap);
+
+            // app/src/main/assets
+            FileOutputStream fos = null;
+            OutputStreamWriter writer = null;
+            try {
+                FileObject resource = filer.createResource(StandardLocation.CLASS_OUTPUT, "", OUTPUT_FILE_NAME);
+                String resourcePath = resource.toUri().getPath();
+                messager.printMessage(Diagnostic.Kind.NOTE, "resourcePath: "+resourcePath);
+                String appPath = resourcePath.substring(0, resourcePath.indexOf("app")+4);
+                String assetsPath = appPath + "src/main/assets/";
+
+                File dir = new File(assetsPath);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                File outputFile = new File(dir, OUTPUT_FILE_NAME);
+                if (outputFile.exists()) {
+                    outputFile.delete();
+                }
+                outputFile.createNewFile();
+                String content = JSON.toJSONString(destMap);
+                fos = new FileOutputStream(outputFile);
+                writer = new OutputStreamWriter(fos);
+                writer.write(content);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private void handleDestination(Set<? extends Element> elements, Class<? extends Annotation> annotationClazz, HashMap<String, JSONObject> destMap) {
+        for(Element element : elements) {
+            TypeElement typeElement = (TypeElement) element;
+            String pageUrl = null;
+            String clazzName = typeElement.getQualifiedName().toString();
+            int id = Math.abs(clazzName.hashCode());
+            boolean needLogin = false;
+            boolean asStarter = false;
+            boolean isFragment = false;
+
+            Annotation annotation = typeElement.getAnnotation(annotationClazz);
+            if (annotation instanceof FragmentDestination) {
+                FragmentDestination dest = (FragmentDestination) annotation;
+                pageUrl = dest.pageUrl();
+                needLogin = dest.needLogin();
+                asStarter = dest.asStarter();
+                isFragment = true;
+            } else if (annotation instanceof ActivityDestination) {
+                ActivityDestination dest = (ActivityDestination) annotation;
+                pageUrl = dest.pageUrl();
+                needLogin = dest.needLogin();
+                asStarter = dest.asStarter();
+                isFragment = false;
+            }
+
+            if (destMap.containsKey(pageUrl)) {
+                messager.printMessage(Diagnostic.Kind.ERROR, "不同的页面不允许使用相同pageUrl "+clazzName);
+            } else {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("id", id);
+                jsonObject.put("clazzName", clazzName);
+                jsonObject.put("pageUrl", pageUrl);
+                jsonObject.put("needLogin", needLogin);
+                jsonObject.put("asStarter", asStarter);
+                jsonObject.put("isFragment", isFragment);
+                destMap.put(pageUrl, jsonObject);
+            }
+        }
+    }
+}
